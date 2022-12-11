0f930973f28ce005a91b3ebe47a908a1ed53ee28
kuzmichoa <117940361+kuzmichoa@users.noreply.github.com>
2022-12-11 21:04:32 +0300

    Add files via upload

---
+++ Лабораторная работа 5/task_1.py.py
@@ -0,0 +1,5 @@
+# TODO решить с помощью list comprehension и распечатать его
+from pprint import pprint
+dict = [{'bin': bin(i), 'dec': i, 'hex': hex(i), 'oct': oct(i)} for i in range(16)]
+pprint(dict)
+# task 1
---
+++ Лабораторная работа 5/task_3.py.py
@@ -0,0 +1,14 @@
+import random
+
+def get_unique_list_numbers() -> list[int]:
+    list_ = list(range(-10, 11))
+    list_uniq = random.sample(list_, 15)
+    return list_uniq
+
+    ...  # TODO написать функцию для получения списка уникальных целых чисел
+
+
+list_unique_numbers = get_unique_list_numbers()
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
+# task 3
